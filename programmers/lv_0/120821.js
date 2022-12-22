067edf1e6f76fbcdd51f21a5fe3342c116b67709
jueun11 <jrangkim11@gmail.com>
2022-12-22 14:42:59 +0900

    배열메소드 reverse활용

---
+++ programmers/lv_0/120821.js
@@ -0,0 +1,6 @@
+function solution(num_list) {
+  return num_list.reverse();
+}
+//배열메소드중 reverse();를 사용하는 간단한 문제였다. 배열 메소드 종류 확인용 문제인듯 하다.
+//타 풀이중 push를 사용해 기존배열의 마지막 숫자부터 차곡차곡 추가한 새 배열을 만드는 방식도 있었으나, 해당 문제의 목적은
+//reverse활용이라 생각하여 자세히 적지않는다.
