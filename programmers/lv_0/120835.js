6a360714182271d98c33930982e411a7130f6750
jueun11 <99877689+jueun11@users.noreply.github.com>
2023-01-18 15:57:14 +0900

    Ordering medical treatment
    원본 배열 유지하면서 새 배열 복사

--- programmers/lv_0/120835.js
+++ programmers/lv_0/120835.js
@@ -5,3 +5,11 @@ function solution(emergency) {
   //new Array써도 테케 2번오류남
   return emergency.map((el) => arr.indexOf(el)+1);
 }
+
+// *다수풀이
+function solution(emergency) {
+  let sorted = emergency.slice().sort((a,b)=>b-a);
+  //slice()는 지정 범위 (시작,끝)까지 복사한다. 범위를 지정하지 않을시 전부 복사.
+  return emergency.map(v=>sorted.indexOf(v)+1);
+  //이부분은 동일
+}
